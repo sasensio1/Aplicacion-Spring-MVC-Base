e0299adb9e39424c3f42b712545bbd3a5a5ffa49
sasensioDTUser <sasensio@10.0.2.15>
2017-05-04 12:09:21 +0200

    sasensio: Initial Commit

---
+++ src/main/java/com/enelnotificacionesproactjava/controller/EnelNotificacionController.java
@@ -0,0 +1,21 @@
+package com.enelnotificacionesproactjava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EnelNotificacionController {
+
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		System.out.println(message);
+		return null;
+	}
+}
