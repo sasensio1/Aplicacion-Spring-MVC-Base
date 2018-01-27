68d41e80ffd4947a2f034a8be42313aedbca972e
Sergio <Sergio@DESKTOP-MVPRIN3>
2018-01-27 06:37:42 +0100

    sasensio: Remodelación de aplicación

--- src/main/java/com/springBasicProject/controller/SpringBasicProjectController.java
+++ src/main/java/com/springBasicProject/controller/SpringBasicProjectController.java
@@ -1,16 +1,16 @@
-package com.enelnotificacionesproactjava.controller;
+package com.springBasicProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class EnelNotificacionController {
+public class SpringBasicProjectController {
 
 	
-	@RequestMapping("/welcome")
+	@RequestMapping(value ="/private/welcome",method = RequestMethod.GET)
 	public ModelAndView helloWorld() {
  
 		String message = "<br><div style='text-align:center;'>"
